62f05fc480ea5944545b22e0f21f60c92fa09a91
Matt Vaughn <mwvaughn@amazon.com>
2022-10-17 15:39:53 -0400

    Fixed categories. Added tag inference from title and abstract. Untangled the spaghetti. Skip media download if exists.

--- scripts/blog_csv_to_posts.py
+++ scripts/blog_csv_to_posts.py
@@ -3,6 +3,7 @@
 import argparse
 import csv
 import os
+from random import SystemRandom
 from unicodedata import category
 import pytz
 import re
@@ -29,62 +30,75 @@ def slugify(s) -> str:
     return s
 
 
-def download_image(url:str, file_name:str, headers:dict=None):
-    return True
+def download_image(url: str, file_name: str, headers: dict = None, force=False):
 
+    # Downloads the flavor image for a blgo post
+    #
     if headers is None:
         headers = {}
-    # Send GET request
-    response = requests.get(url, headers=headers)
     # Save the image
-    if response.status_code == 200:
-        with open(os.path.join(IMAGE_ASSETS, file_name), "wb") as f:
-            f.write(response.content)
-    else:
-        print(response.status_code)
-
-def transform_categories(categories: List) -> List[str]:
-
-    alloweds = []
-
-    # Implement allow list
-    CATEGORIES_ALLOW = [
-        "Artificial Intelligence",
-        "AWS Batch",
-        "AWS ParallelCluster",
-        "EFA",
-        "Amazon FSx for Lustre",
-        "Life Sciences",
-        "NICE DCV",
-        "Financial Services",
-    ]
-    for c in categories:
-        if c in CATEGORIES_ALLOW:
-            alloweds.append(c)
-
-    # if 'Artificial Intelligence' in alloweds:
-    #     raise SystemError(alloweds)
-
-    # Strip these out of categories
-    PREFIXES = []
-
-    # Tranform K => V
-    TRANSFORMS = {
-        "Artificial Intelligence": "AI/ML",
-        "EFA": "Amazon EFA",
-    }
-
-    # Some categories should be replaced with others, defined
-    # in TRANSFORMS
-    transformed = []
-    for c in alloweds:
-        if c in TRANSFORMS:
-            tag = TRANSFORMS[c]
+    destination_path = os.path.join(IMAGE_ASSETS, file_name)
+    if not os.path.exists(destination_path) or force is True:   
+        # Send GET request
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            with open(os.path.join(IMAGE_ASSETS, file_name), "wb") as f:
+                f.write(response.content)
         else:
-            tag = c
-        transformed.append(tag)
+            print(response.status_code)
 
-    return transformed
+
+def infer_tags(text: str, title:str=None) -> List[str]:
+    # Infers tags from text with a regex
+
+    if title is not None:
+        text = title + ' ' + text
+
+    PATTERNS = {
+        "AI/ML": [r"AI/ML", r"machine learning", r"neural network", r'\bML\b'],
+        "Arm": [r'\bArm\b', r'Graviton'],
+        "NICE DCV": [r'NICE DCV', r'\bDCV'],
+        "EFA": [r"EFA", r"Elastic Fabric Adapter"],
+        "ParallelCluster": [r"ParallelCluster"],
+        "Batch": [r"Batch"],
+        "Slurm": [r"Slurm"],
+        "HPC": [r"HPC", r"High Performance Computing"],
+        "Climate/Environment/Weather": [r"Climate", r"Weather", r"Meteoro"],
+        "CAE/CFD": [r"engineering", r'\bCAE\b', r"CFD", r"fluid dynamics"],
+        "Life Sciences": [
+            r"Genomics",
+            r"Sequencing",
+            r"NGS",
+            r"Cryo-EM",
+            r"CryoEM",
+            r"Parabricks",
+            r"GROMACS",
+            r"small molecule",
+            r"protein",
+            r"DNA",
+            r"bioinformatics",
+            r"pharmaceutical",
+            r"genetic",
+            r"medicine",
+            r"medical",
+            r"biotech",
+            r"molecular dynamics",
+        ],
+        "Financial Services": [r"financial", r"banking"],
+        "Quantum technologies": [r'quantum'],
+        "Simulation": [r'simulation'],
+        "Media": [r'media', r'animation', r'film', r'entertainment'],
+        "Modeling": [r'modeling', r'model']
+    }
+    tags = []
+    for t, pats in PATTERNS.items():
+        for p in pats:
+            rx = re.compile(p)
+            if rx.search(text, re.IGNORECASE):
+                tags.append(t)
+                continue
+    return tags
+
 
 def transform_tags(tags: List) -> List[str]:
     # Strip these out of tags
@@ -101,12 +115,10 @@ def transform_tags(tags: List) -> List[str]:
 
     # Omit these tags entirely
     FILTERED = [
-        "Advanced 300",
-        "Amplify",
+        "Advanced (300)",
         "Announcements",
         "Cloud Digital Interface",
         "Customer Solutions",
-        "Launch",
         "Partner Network",
         "Thought Leadership",
     ]
@@ -135,6 +147,7 @@ def transform_tags(tags: List) -> List[str]:
             filtered_tags.append(tag)
     return filtered_tags
 
+
 def date_published_to_iso(date_published: str) -> str:
     # extract from string date
     dt = datetime.strptime(date_published, "%d %b %Y")
@@ -146,30 +159,38 @@ def date_published_to_iso(date_published: str) -> str:
     return dt3
 
 
-def extract_categories(categories_str: str) -> List[str]:
-    CATEGORIES_ALLOW = [
-        "Artificial Intelligence",
-        "AWS Batch",
-        "AWS ParallelCluster",
-        "EFA",
-        "Amazon FSx for Lustre",
-        "Life Sciences",
-        "NICE DCV",
-        "Financial Services",
-    ]
-    
-    tags = extract_tags(categories_str, transform=False)
-    categories = transform_categories(tags)
+def extract_categories(tags: List[str]) -> List[str]:
 
-    return categories
+    # Given a list of tags, some of which are inferred, 
+    # classify the blog into a category
 
+    # Tranform Tag => Category
+    TRANSFORMS = {
+        "Batch": "Amazon Batch",
+        "ParallelCluster": "AWS ParallelCluster",
+        "EFA": "Amazon Elastic Fabric Adapter",
+        "NICE DCV": "Amazon NICE DCV",
+        "AI/ML": "AI/ML",
+        "CAE/CFD": "CAE/CFD",
+        "Climate/Environment/Weather": "Climate/Environment/Weather",
+        "Financial Services": "Financial Services",
+        "Life Sciences": "Life Sciences"
+    }
+
+    # Match and sometimes transform tags to categories
+    mapped_tags = []
+    for tag in tags:
+        if tag in TRANSFORMS:
+            tag = TRANSFORMS[tag]
+            mapped_tags.append(tag)
+
+    return mapped_tags
 
 def extract_tags(categories_str: str, transform=True) -> List[str]:
     tags = categories_str.split(" | ")
     if transform:
         tags = transform_tags(tags)
     return tags
-
 
 def row_to_blog(row):
     # Build up blog, a dictionary used to populate Jinja templates
@@ -179,20 +200,34 @@ def row_to_blog(row):
         "thumbnail": row["image"],
         "thumbnail_filename": slugify(row["image"]) + ".png",
         "url": row["URL"],
-        "categories": extract_categories(row["categories"]),
-        "tags": extract_tags(row["categories"]),
         "date_published": date_published_to_iso(row["datePublished"]),
         "filename": slugify(row["title"]) + ".md",
     }
     # Compute Image path (or default)
     if row["image"] != "":
         parts = urlparse(blog["thumbnail"])
-        blog["thumbnail_filename"] = os.path.join(IMAGE_ASSETS_SUBDIR, os.path.basename(parts.path))
+        blog["thumbnail_filename"] = os.path.join(
+            IMAGE_ASSETS_SUBDIR, os.path.basename(parts.path)
+        )
     else:
         # TODO - replace with something sensible
-        blog["thumbnail_filename"] = os.path.join(IMAGE_ASSETS_SUBDIR, 'dalle-hpc-01.png')
+        blog["thumbnail_filename"] = os.path.join(
+            IMAGE_ASSETS_SUBDIR, "dalle-hpc-01.png"
+        )
+
+    # tags
+    tags = extract_tags(row["categories"])
+    tags.extend(infer_tags(blog['excerpt'], blog['title']))
+    tags = list(set(tags))
+    blog['tags'] = tags
     # Add "blog" to tags
     blog["tags"].append("hpcblog")
+
+    # categories
+    categories = extract_categories(tags)
+    # raise SystemError(categories)
+    blog['categories'] = categories
+
     return blog
 
 
@@ -213,9 +248,7 @@ def main(values):
                 print(data["filename"])
             # Download the illustrative image associated with each post
             if data["thumbnail"] != "":
-                download_image(
-                    data["thumbnail"], data["thumbnail_filename"]
-                )
+                download_image(data["thumbnail"], data["thumbnail_filename"])
 
 
 if __name__ == "__main__":
